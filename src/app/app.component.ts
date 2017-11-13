0d12092b70f29a736837a7eb123c28d0a902c132
Bruno Krebs <bruno.krebs@auth0.com>
2017-11-13 21:03:10 -0200

    chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewChecked {
+  messages: Array<MessageEvent> = [];
+  private ws = new WebSocket('ws://localhost:8080/ws/messages');
+  @ViewChild('chatHistory') private chatHistory: ElementRef;
+
+  constructor() {
+    this.ws.onmessage = (me => {
+      const data = JSON.parse(me.data) as MessageEvent;
+      this.messages.push(data);
+    });
+  }
+
+  ngAfterViewChecked() {
+    this.chatHistory.nativeElement.scrollTop = this.chatHistory.nativeElement.scrollHeight;
+  }
+}
+
+interface MessageEvent {
+  message: string;
+  when: string;
 }
