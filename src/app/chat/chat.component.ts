f3e862be31ea2d41bbfef866024a917b3a038149
Bruno Krebs <bruno.krebs@auth0.com>
2017-11-14 10:58:23 -0200

    Managing login logout

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,16 +1,20 @@
 import {AfterViewChecked, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+import {AuthService} from '../auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements AfterViewChecked {
+export class ChatComponent implements AfterViewChecked, OnInit {
   messages: Array<MessageEvent> = [];
   private ws: WebSocket;
+  authSubscription: Subscription;
   @ViewChild('chatHistory') private chatHistory: ElementRef;
 
-  constructor() {
+  constructor(private auth: AuthService, private router: Router) {
     this.ws = new WebSocket('ws://localhost:8080/ws/messages');
     this.ws.onmessage = (me => {
       const data = JSON.parse(me.data) as MessageEvent;
@@ -20,6 +24,16 @@ export class ChatComponent implements AfterViewChecked {
 
   ngAfterViewChecked() {
     this.chatHistory.nativeElement.scrollTop = this.chatHistory.nativeElement.scrollHeight;
+  }
+
+  ngOnInit() {
+    this.authSubscription = this.auth.loggedIn$.subscribe(loggedIn => {
+      if (!loggedIn) {
+        this.ws.close();
+        this.ws = null;
+        this.router.navigate(['']);
+      }
+    });
   }
 }
 
